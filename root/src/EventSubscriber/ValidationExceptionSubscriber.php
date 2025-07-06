0207786361837640714ddf5dfc05ed731aa3e08c
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-07-06 20:22:54 +0200

    Remove unused import

--- root/src/EventSubscriber/ValidationExceptionSubscriber.php
+++ root/src/EventSubscriber/ValidationExceptionSubscriber.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
-use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 class ValidationExceptionSubscriber implements EventSubscriberInterface
 {
