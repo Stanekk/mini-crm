b3551e50b0f6fe27e71a792e12f5a6d260d8a1ec
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-07-07 18:54:23 +0200

    Return better errors for validation

--- root/src/EventSubscriber/ValidationExceptionSubscriber.php
+++ root/src/EventSubscriber/ValidationExceptionSubscriber.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 class ValidationExceptionSubscriber implements EventSubscriberInterface
 {
@@ -13,16 +14,24 @@ class ValidationExceptionSubscriber implements EventSubscriberInterface
     {
         $exception = $event->getThrowable();
 
-        if (!$exception instanceof UnprocessableEntityHttpException) {
-            return;
+        if ($exception instanceof UnprocessableEntityHttpException) {
+            $previous = $exception->getPrevious();
+
+            if ($previous instanceof ValidationFailedException) {
+                $violations = $previous->getViolations();
+
+                $errors = [];
+
+                foreach ($violations as $violation) {
+                    $propertyPath = $violation->getPropertyPath();
+                    $message = $violation->getMessage();
+                    $errors[$propertyPath][] = $message;
+                }
+
+                $response = new JsonResponse(['errors' => $errors], 422);
+                $event->setResponse($response);
+            }
         }
-
-        $errors = $exception->getMessage();
-        $errorsArray = explode("\n", $errors);
-
-        $response = new JsonResponse(['errors' => $errorsArray], 422);
-
-        $event->setResponse($response);
     }
 
     public static function getSubscribedEvents(): array
