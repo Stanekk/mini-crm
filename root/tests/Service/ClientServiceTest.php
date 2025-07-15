e4b94fb9eb08cf441212630a518e04921bd063cb
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-07-15 18:23:34 +0200

    Add test Create Client With Company

--- root/tests/Service/ClientServiceTest.php
+++ root/tests/Service/ClientServiceTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Service;
 
 use App\Dto\CreateClientRequestDto;
 use App\Entity\Client;
+use App\Entity\Company;
 use App\Service\ClientService;
 use App\Service\CompanyService;
 use Doctrine\ORM\EntityManager;
@@ -37,6 +38,27 @@ class ClientServiceTest extends TestCase
         $this->assertSame('email@email.com', $client->getEmail());
         $this->assertSame('+48555111222', $client->getPhone());
         $this->assertNull($client->getCompany());
+    }
+
+    public function testCreateClientWithCompany(): void
+    {
+        $dto = new CreateClientRequestDto('firstName', 'lastName', 'email@email.com', '+48555111222', 123);
+
+        $company = new Company();
+        $company->setName('Extra company');
+        $this->companyService->method('getCompanyById')->with(123)->willReturn($company);
+
+        $this->entityManager->expects($this->once())->method('persist')->with($this->isInstanceOf(Client::class));
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $client = $this->clientService->create($dto);
+
+        $this->assertInstanceOf(Client::class, $client);
+        $this->assertSame('firstName', $client->getFirstName());
+        $this->assertSame('lastName', $client->getLastName());
+        $this->assertSame('email@email.com', $client->getEmail());
+        $this->assertSame('+48555111222', $client->getPhone());
+        $this->assertSame($company, $client->getCompany());
     }
 
 
