d1aa90b09429836b0bd186938bdda5742793c2a7
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-07-19 00:07:10 +0200

    Add task dto and mapper

---
+++ root/src/Dto/TaskDto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Dto;
+
+use App\Enum\TaskStatus;
+
+final readonly class TaskDto
+{
+    public int $id;
+    public string $name;
+    public ?string $description;
+    public ?\DateTimeImmutable $dueDate;
+    public \DateTimeImmutable $createdAt;
+    public ?\DateTimeImmutable $updatedAt;
+    public ?UserDto $assignedTo;
+    public ?ClientDto $client;
+    public ?CompanyDto $company;
+    public TaskStatus $status;
+
+    public function __construct(int $id, string $name, ?string $description, \DateTimeImmutable $dueDate, \DateTimeImmutable $createdAt, ?\DateTimeImmutable $updatedAt, ?UserDto $assignedTo, ?ClientDto $client, ?CompanyDto $company, TaskStatus $status)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->dueDate = $dueDate;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+        $this->assignedTo = $assignedTo;
+        $this->client = $client;
+        $this->company = $company;
+        $this->status = $status;
+    }
+}
---
+++ root/src/Mapper/TaskMapper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mapper;
+
+use App\Dto\TaskDto;
+use App\Entity\Task;
+
+class TaskMapper
+{
+    private UserMapper $userMapper;
+    private ClientMapper $clientMapper;
+    private CompanyMapper $companyMapper;
+
+    public function __construct(UserMapper $userMapper, ClientMapper $clientMapper, CompanyMapper $companyMapper)
+    {
+        $this->userMapper = $userMapper;
+        $this->clientMapper = $clientMapper;
+        $this->companyMapper = $companyMapper;
+    }
+
+    public function toDto(Task $task): TaskDto
+    {
+        $assignedToUser = $task->getAssignedTo();
+        if ($assignedToUser) {
+            $assignedToUser = $this->userMapper->toDto($assignedToUser);
+        }
+        $client = $task->getClient();
+        if ($client) {
+            $client = $this->clientMapper->toDto($client);
+        }
+        $company = $task->getCompany();
+        if ($company) {
+            $company = $this->companyMapper->toDto($company);
+        }
+
+        return new TaskDto(
+            $task->getId(),
+            $task->getName(),
+            $task->getDescription(),
+            $task->getDueDate(),
+            $task->getCreatedAt(),
+            $task->getUpdatedAt(),
+            $assignedToUser,
+            $client,
+            $company,
+            $task->getStatus(),
+        );
+    }
+}
