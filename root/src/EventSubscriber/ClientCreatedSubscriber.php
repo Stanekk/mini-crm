d7bead83352d1ea3c4f7cad05f422e1e2461e05b
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-07-29 20:42:51 +0200

    Send client welcome email in event subscriber

--- root/src/EventSubscriber/ClientCreatedSubscriber.php
+++ root/src/EventSubscriber/ClientCreatedSubscriber.php
@@ -3,10 +3,18 @@
 namespace App\EventSubscriber;
 
 use App\Event\ClientCreatedEvent;
+use App\Service\EmailService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class ClientCreatedSubscriber implements EventSubscriberInterface
 {
+    private EmailService $emailService;
+
+    public function __construct(EmailService $emailService)
+    {
+        $this->emailService = $emailService;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -17,5 +25,6 @@ class ClientCreatedSubscriber implements EventSubscriberInterface
     public function onClientCreated(ClientCreatedEvent $event): void
     {
         $client = $event->getClient();
+        $this->emailService->sendClientWelcomeEmail($client);
     }
 }
