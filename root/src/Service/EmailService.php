bc12ec42a3a6ce603fff0c6e738676e17f53979f
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-07-29 20:41:20 +0200

    Add email service

---
+++ root/src/Service/EmailService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Client;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
+class EmailService
+{
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    private function prepareBaseEmail(string $email, string $from, string $subject, string $templatePath): TemplatedEmail
+    {
+        $email = (new TemplatedEmail())->from($from)
+            ->to(new Address($email))
+            ->subject($subject)
+            ->htmlTemplate($templatePath);
+
+        return $email;
+    }
+
+    public function sendClientWelcomeEmail(Client $client): void
+    {
+        $preparedEmail = $this->prepareBaseEmail($client->getEmail(), 'mini@crm.pl', 'Welcome!', 'emails/client_welcome.html.twig');
+        $preparedEmail->context(['clientName' => $client->getEmail()]);
+        $this->mailer->send($preparedEmail);
+    }
+}
