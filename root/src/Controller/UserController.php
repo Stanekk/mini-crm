c69ee949f95d80a5007ea010c08a3ecf2eb9c595
Rafal Stankiewicz <rafal.stankiewicz@made.media>
2025-08-04 20:44:53 +0200

    Add delete user route

--- root/src/Controller/UserController.php
+++ root/src/Controller/UserController.php
@@ -3,13 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Enum\Role;
 use App\Mapper\UserMapper;
 use App\Repository\UserRepository;
 use App\Service\PaginationService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -17,12 +20,14 @@ class UserController extends AbstractController
     private UserRepository $userRepository;
     private UserMapper $userMapper;
     private PaginationService $paginationService;
+    private UserService $userService;
 
-    public function __construct(UserRepository $repository, UserMapper $userMapper, PaginationService $paginationService)
+    public function __construct(UserRepository $repository, UserMapper $userMapper, PaginationService $paginationService, UserService $userService)
     {
         $this->userRepository = $repository;
         $this->userMapper = $userMapper;
         $this->paginationService = $paginationService;
+        $this->userService = $userService;
     }
 
     #[Route('/api/users', name: 'app_users_list', methods: ['GET'])]
@@ -36,6 +41,22 @@ class UserController extends AbstractController
         );
 
         return new JsonResponse($result, Response::HTTP_OK);
+    }
+
+    #[Route('/api/users/{id}', name: 'app_user_delete', methods: ['DELETE'])]
+    public function delete(int $id): JsonResponse
+    {
+        $this->denyAccessUnlessGranted(Role::Admin->value);
+
+        $user = $this->userService->getUserById($id);
+
+        if (!$user) {
+            throw new NotFoundHttpException('User not found');
+        }
+
+        $this->userService->deleteUser($user);
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/api/me', name: 'app_me', methods: ['GET'])]
--- root/src/Service/UserService.php
+++ root/src/Service/UserService.php
@@ -4,14 +4,18 @@ namespace App\Service;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class UserService
 {
     private EntityManagerInterface $entityManager;
+    private Security $security;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
     {
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
 
     public function getUserById(int $id): ?User
@@ -22,5 +26,15 @@ class UserService
         }
 
         return $user;
+    }
+
+    public function deleteUser(User $user): void
+    {
+        $currentUser = $this->security->getUser();
+        if ($currentUser->getId() === $user->getId()) {
+            throw new AccessDeniedHttpException('You cannot delete yourself.');
+        }
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
     }
 }
